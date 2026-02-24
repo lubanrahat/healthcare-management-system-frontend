cbaca910d342eddab031d1e35bd313852870b1b1
lubanrahat <lubanrahat.dev@gmail.com>
2026-02-24 17:39:41 +0600

    add new pages for change password, my profile, book appointments, health records, my appointments, my prescriptions, patient dashboard, and payment success

---
+++ src/app/(dashboardLayout)/(patientRouteGroup)/(patientDashboardLayout)/dashboard/my-prescriptionts/page.tsx
@@ -0,0 +1,10 @@
+export default function MyPrescriptiontsPage() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold"> My Prescription Page</h1>
+      <p className="mt-2 text-muted-foreground">
+        Page content goes here
+      </p>
+    </main>
+  )
+}
---
+++ src/app/(dashboardLayout)/(patientRouteGroup)/(patientDashboardLayout)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function PatientDashboardPage() {
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold">Page Title</h1>
+      <p className="mt-2 text-muted-foreground">
+        Page content goes here
+      </p>
+    </main>
+  )
+}
